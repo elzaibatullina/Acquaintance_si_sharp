9a7da98caf209c66d1638bd72fdbfbea208ad370
elzaibatullina <elza.ibatullina.89@gmail.com>
2022-11-28 02:39:47 +0500

    Решили первую задачу.Вывести квадрат числа

---
+++ Seminar01/EX01/Program.cs
@@ -0,0 +1,8 @@
+﻿// 0. Вывести квадрат числа
+Console.WriteLine("start"); // Начало
+
+int number = 11; // Определили целое число
+int result = number * number; // Вычисление квадрата числа
+Console.WriteLine (result); // Вывод результата
+
+Console.WriteLine("end"); // Окончание
