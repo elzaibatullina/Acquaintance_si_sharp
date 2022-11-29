657786bf33a9032e2262b8cef4f94060d65a5954
elzaibatullina <elza.ibatullina.89@gmail.com>
2022-11-30 02:41:24 +0500

    Обновлен readme для домашки

---
+++ Hom/EX01/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 0: Напишите программу, 
+// которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+//start
+// int a = -9;
+// int b = -3;
+// int max = 0;
+
+// if (a > b)
+// {
+//   max = a;
+// }
+// else
+// {
+//   max = b;
+// }
+
+// Console.WriteLine(max);
+int a = -9;
+int b = -3;
+int max = a;
+
+// if (b > max)
+// {
+//   max = b;
+// }
+
+if (b > max)
+{
+  max = b;
+}
+
+
+Console.WriteLine(max);
+
+
+
+
+//end
---
+++ Hom/EX020/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+ Console.WriteLine("start");
+
+ int number = 4;
+ int result = number;
+
+   Console.WriteLine (result); 
+
+ if (number % 2 == 0)
+ {
+   Console.WriteLine ("число чётное");
+ }  
+
+  else
+ {
+   Console.WriteLine ("число нечетное"); 
+ } 
+
+   Console.WriteLine("end"); 
+
+
